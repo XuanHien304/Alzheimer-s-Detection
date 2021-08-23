5f22ec13059ded4e888c13d756dfb3e733997419
XuanHien304 <vuxuanhien304@gmail.com>
2021-08-23 14:59:53 +0700

    add test.py and adjust train.py

---
+++ test.py
@@ -0,0 +1,31 @@
+import numpy as np
+import joblib
+import os
+from sklearn.preprocessing import MinMaxScaler 
+
+class Alzheimer_Detector():
+
+    def __init__(self):
+        best_model = os.listdir('./models')
+        self.model = joblib.load('./models' + '/' + str(best_model[0]))
+
+    def user_predict(self, info):
+        '''
+            Input: feature of the patient
+
+            Output: Alzheimer's Demented or Not
+
+        '''
+        info_np = np.array(info).reshape(1,-1)
+        scaler = MinMaxScaler().fit(info_np)
+        info_np = scaler.transform(info_np)
+        prediction = self.model.predict(info_np)
+        
+        if prediction[0] == 0:
+            output = "Nondemented"
+        else:
+            output = "Demented" 
+        return output 
+
+
+

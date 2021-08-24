41a9cb36443829589e5683d791385602d9ea6443
XuanHien304 <vuxuanhien304@gmail.com>
2021-08-24 21:14:22 +0700

    update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -8,27 +8,26 @@ def main():
     st.text('')
 
     # Input feature
-    gender = st.text_input('Enter sex: Male: 1, Female: 0')
-    age = st.text_input('Enter age of patient')
-    educ = st.text_input('Year of education')
-    ses = st.text_input('Socioeconomic Status')
-    mmse = st.text_input('Mini Mental State Examination')
-    etiv = st.text_input('Estimated Total Intracranial Volume')
-    nwbv = st.text_input('Normalize Whole Brain Volume')
-    asf = st.text_input('Atlas Scaling Factor')
+    gender = st.number_input('Enter sex: Male: 1, Female: 0')
+    age = st.number_input('Enter age of patient')
+    educ = st.number_input('Year of education')
+    ses = st.number_input('Socioeconomic Status')
+    mmse = st.number_input('Mini Mental State Examination')
+    etiv = st.number_input('Estimated Total Intracranial Volume')
+    nwbv = st.number_input('Normalize Whole Brain Volume')
+    asf = st.number_input('Atlas Scaling Factor', step=1.,format="%.2f")
 
     patient_input = [gender, age, educ, ses, mmse, etiv, nwbv, asf]
-    patient_input = map(float, patient_input)
 
     # Make prediction
     model = Alzheimer_Detector()
     predict = model.user_predict(patient_input)
-
+    print(patient_input)
     if st.button("Make prediction"):
         if predict == '0':
             st.success('Nondemented')
         else:
-            st.danger('Demented')
+            st.warning('Demented')
 
 if __name__ == '__main__':
     main()
